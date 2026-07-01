d0d8c99a266a0ffe20e3527cc5bcbffac8180411
chairunnisaprasanti <chairunnisaprasanti06@gmail.com>
2026-07-02 00:10:27 +0700

    Initial Laravel setup and countries module

---
+++ app/Models/Country.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    protected $fillable = [
+        'country_name',
+        'country_code',
+        'capital',
+        'region',
+        'currency_code',
+        'currency_name',
+        'latitude',
+        'longitude',
+        'flag'
+    ];
+}
